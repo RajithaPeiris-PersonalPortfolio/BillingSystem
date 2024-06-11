3618991b94f10beedacf0fbeaa97aafb72821ce0
RAJITHA_SD1 <sd3@crysbro.net>
2024-06-11 16:26:46 +0530

    first commit

---
+++ LOGIN.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace BillingSystem
+{
+    partial class LOGIN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(108, 43);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(165, 20);
+            this.txt_user.TabIndex = 3;
+            this.txt_user.TextChanged += new System.EventHandler(this.txt_user_TextChanged);
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(108, 89);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(165, 20);
+            this.txt_pass.TabIndex = 4;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(42, 121);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(80, 24);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(138, 121);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(80, 23);
+            this.btn_exit.TabIndex = 6;
+            this.btn_exit.Text = "EXIT";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(238, 122);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(80, 23);
+            this.btn_clear.TabIndex = 7;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // LOGIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(326, 168);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LOGIN";
+            this.Text = "LOGIN";
+            this.Load += new System.EventHandler(this.LOGIN_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
---
+++ LOGIN.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CafeManagement;
+
+
+
+namespace BillingSystem
+{
+    public partial class LOGIN : Form
+    {
+        public LOGIN()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=SD3;Initial Catalog=Seed_Cafe;Integrated Security=True");
+        
+        private void LOGIN_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string username, user_password;
+
+            username = txt_user.Text;
+            user_password = txt_pass.Text;
+           
+
+            try
+            {
+                string query = "SELECT * FROM Login_new WHERE username ='"+txt_user.Text+"'AND Password ='"+txt_pass.Text+"'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    username = txt_user.Text;
+                    user_password = txt_pass.Text;
+
+                    // page that needed to be load nex
+                    Menuform form1 = new Menuform();
+                    form1.Show();
+                    this.Hide();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Login details","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    txt_user.Clear();
+                    txt_pass.Clear();
+
+                    // to focus username
+                    txt_user.Focus();
+
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_user.Clear();
+            txt_pass.Clear();
+
+            txt_user.Focus();
+        }
+
+        private void txt_user_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
